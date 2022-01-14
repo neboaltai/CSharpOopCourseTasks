aa778b99af43ec3aba7d7ac88eec54d7c8d921d8
Misha Blinov <spasibo1989@gmail.com>
2022-01-14 16:07:57 +0700

    Добавлены методы получения длины диапазона и проверки принадлежности числа диапазону

--- RangeTask/Program.cs
+++ RangeTask/Program.cs
@@ -6,7 +6,25 @@ namespace RangeTask
     {
         static void Main(string[] args)
         {
+            Range range = new Range(0, 0);
 
+            Console.WriteLine($"Задайте непрерывный числовой диапазон ({range.From}, {range.To})");
+            Console.Write("начальное значение: ");
+            range.From = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("конечное значение: ");
+            range.To = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine("Длина диапазона: " + range.GetLength());
+
+            Console.WriteLine();
+            Console.Write("Введите число на прямой: ");
+            double number = Convert.ToDouble(Console.ReadLine());
+
+            string inside = range.IsInside(number) ? "принадлежит" : "не принадлежит";
+            Console.WriteLine();
+            Console.WriteLine($"Число {number} {inside} заданному диапазону");
         }
     }
 }
---
+++ RangeTask/Range.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RangeTask
+{
+    class Range
+    {
+        public double From { get; set; }
+
+        public double To { get; set; }
+
+        public Range(double from, double to)
+        {
+            From = from;
+            To = to;
+        }
+
+        public double GetLength()
+        {
+            return To - From;
+        }
+
+        public bool IsInside(double number)
+        {
+            return number - From >= 0 && To - number >= 0;
+        }
+    }
+}
