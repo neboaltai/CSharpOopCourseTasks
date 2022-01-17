03210ad1e95ff44f8fe47f4555aee92553a13c25
Misha Blinov <spasibo1989@gmail.com>
2022-01-17 14:51:40 +0700

    Добавлены методы получения пересечения, объединения, разности интервалов и их печати

--- RangeTask/Program.cs
+++ RangeTask/Program.cs
@@ -4,27 +4,80 @@ namespace RangeTask
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Print(params Range[] ranges)
         {
-            Range range = new Range(0, 0);
+            bool isEmpty = true;
 
-            Console.WriteLine($"Задайте непрерывный числовой диапазон ({range.From}, {range.To})");
-            Console.Write("начальное значение: ");
-            range.From = Convert.ToDouble(Console.ReadLine());
+            foreach (Range range in ranges)
+            {
+                if (range != null)
+                {
+                    Console.Write($" ({range.From}, {range.To})");
 
-            Console.Write("конечное значение: ");
-            range.To = Convert.ToDouble(Console.ReadLine());
+                    isEmpty = false;
+                }
+            }
+
+            if (isEmpty)
+            {
+                Console.Write(" отсутствует");
+            }
 
             Console.WriteLine();
-            Console.WriteLine("Длина диапазона: " + range.GetLength());
+            Console.WriteLine();
+        }
+
+        static void Main(string[] args)
+        {
+            Range range1 = new Range(0, 0);
+
+            Console.WriteLine($"Задайте первый интервал ({range1.From}, {range1.To})");
+            Console.Write("начальное значение: ");
+            range1.From = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("конечное значение: ");
+            range1.To = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine("Длина первого диапазона: " + range1.GetLength());
 
             Console.WriteLine();
             Console.Write("Введите число на прямой: ");
             double number = Convert.ToDouble(Console.ReadLine());
 
-            string inside = range.IsInside(number) ? "принадлежит" : "не принадлежит";
+            string inside = range1.IsInside(number) ? "принадлежит" : "не принадлежит";
+
             Console.WriteLine();
             Console.WriteLine($"Число {number} {inside} заданному диапазону");
+
+            Range range2 = new Range(0, 0);
+
+            Console.WriteLine();
+            Console.WriteLine($"Задайте второй интервал ({range2.From}, {range2.To})");
+            Console.Write("начальное значение: ");
+            range2.From = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("конечное значение: ");
+            range2.To = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.Write("Интервал пересечения:");
+
+            Range intersectionRange = range1.GetIntersection(range2);
+
+            Print(intersectionRange);
+
+            Console.Write("Интервал(ы) объединения:");
+
+            Range[] unionRanges = range1.GetUnion(range2);
+
+            Print(unionRanges);
+
+            Console.Write("Интервал(ы) разности:");
+
+            Range[] differenceRanges = range1.GetDifference(range2);
+
+            Print(differenceRanges);
         }
     }
 }
--- RangeTask/Range.cs
+++ RangeTask/Range.cs
@@ -27,5 +27,53 @@ namespace RangeTask
         {
             return number - From >= 0 && To - number >= 0;
         }
+
+        public Range GetIntersection(Range range)
+        {
+            if (range.From >= To || range.To <= From)
+            {
+                return null;
+            }
+
+            double from = range.From <= From ? From : range.From;
+            double to = range.To <= To ? range.To : To;
+
+            return new Range(from, to);
+        }
+
+        public Range[] GetUnion(Range range)
+        {
+            if (range.From > To || range.To < From)
+            {
+                return new Range[] { this, range };
+            }
+
+            double from = range.From <= From ? range.From : From;
+            double to = range.To <= To ? To : range.To;
+
+            return new Range[] { new Range(from, to) };
+        }
+
+        public Range[] GetDifference(Range range)
+        {
+            Range differenceRange1 = null;
+
+            Range differenceRange2 = null;
+
+            if (range.From < To && range.To > From)
+            {
+                if (From < range.From)
+                {
+                    differenceRange1 = new Range(From, range.From);
+                }
+
+                if (To > range.To)
+                {
+                    differenceRange2 = new Range(range.To, To);
+                }
+            }
+
+            return new Range[] { differenceRange1, differenceRange2 };
+        }
     }
 }
