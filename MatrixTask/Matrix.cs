ffe647eeef98e274812f479acab970cb300b8188
Misha Blinov <spasibo1989@gmail.com>
2022-03-21 14:43:02 +0700

    Реализованы конструкторы класса Matrix

--- MatrixTask/Matrix.cs
+++ MatrixTask/Matrix.cs
@@ -5,6 +5,91 @@ namespace MatrixTask
 {
     class Matrix
     {
-        
+        private Vector[] rowsArray;
+
+        public Matrix(int rowsCount, int columnsCount)
+        {
+            if (rowsCount <= 0)
+            {
+                throw new ArgumentException($"Parameter value {rowsCount} is invalid. The count of components must be > 0", nameof(rowsCount));
+            }
+
+            if (columnsCount <= 0)
+            {
+                throw new ArgumentException($"Parameter value {columnsCount} is invalid. The count of components must be > 0", nameof(columnsCount));
+            }
+
+            rowsArray = new Vector[rowsCount];
+
+            for (int i = 0; i < rowsCount; i++)
+            {
+                rowsArray[i] = new Vector(columnsCount);
+            }
+        }
+
+        public Matrix(double[,] array)
+        {
+            if (array is null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            int rowsCount = array.GetLength(0);
+            int columnsCount = array.GetLength(1);
+
+            rowsArray = new Vector[rowsCount];
+
+            for (int i = 0; i < rowsCount; i++)
+            {
+                rowsArray[i] = new Vector(columnsCount);
+
+                for (int j = 0; j < columnsCount; j++)
+                {
+                    rowsArray[i].SetComponent(j, array[i, j]);
+                }
+            }
+        }
+
+        public Matrix(Vector[] vectors)
+        {
+            if (vectors is null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            int columnsCount = 0;
+
+            rowsArray = new Vector[vectors.Length];
+
+            for (int i = 0; i < vectors.Length; i++)
+            {
+                rowsArray[i] = new Vector(vectors[i]);
+
+                columnsCount = Math.Max(columnsCount, vectors[i].GetSize());
+            }
+
+            for (int i = 0; i < rowsArray.Length; i++)
+            {
+                if (rowsArray[i].GetSize() < columnsCount)
+                {
+                    rowsArray[i].Add(new Vector(columnsCount));
+                }
+            }
+        }
+
+        public Matrix(Matrix matrix)
+        {
+            if (matrix is null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            rowsArray = new Vector[matrix.rowsArray.Length];
+
+            for (int i = 0; i < matrix.rowsArray.Length; i++)
+            {
+                rowsArray[i] = new Vector(matrix.rowsArray[i]);
+            }
+        }
     }
 }
