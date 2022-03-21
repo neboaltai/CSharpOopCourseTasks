3cc6d72e5ce892d4abebfe215da6a5a60f722806
Misha Blinov <spasibo1989@gmail.com>
2022-03-21 20:38:21 +0700

    Добавлена программа в Main()

--- CsvToHtmlConverter/Program.cs
+++ CsvToHtmlConverter/Program.cs
@@ -6,7 +6,27 @@ namespace CsvToHtmlConverter
     {
         static void Main(string[] args)
         {
-            
+            if (args.Length == 2)
+            {
+                CsvToHtmlConverter.ConvertCsvToHtml(args[0], args[1]);
+                return;
+            }
+
+            if (args.Length != 0 && args[0].StartsWith("/?"))
+            {
+                Console.WriteLine("Конвертировать данные из файла формата .csv в таблицу формата .html");
+            }
+            else
+            {
+                Console.WriteLine("Ошибка: неопознанная или неполная командная строка");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("ИСПОЛЬЗОВАНИЕ:");
+            Console.WriteLine("\tCsvToHtmlConverter [источник] [результат]");
+            Console.WriteLine();
+            Console.WriteLine("  источник\tКаталог и/или имя конвертируемого файла.");
+            Console.WriteLine("  результат\tКаталог и/или имя для конечного файла.");
         }
     }
 }
