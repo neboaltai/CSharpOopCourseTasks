1bbc62494d1cb0471453d1bf9af182be4bbbdfbe
Misha Blinov <spasibo1989@gmail.com>
2022-03-22 23:43:10 +0700

    Реализован класс SinglyLinkedListNode<T> Добавлены члены и конструкторы

--- ListTask/SinglyLinkedListNode.cs
+++ ListTask/SinglyLinkedListNode.cs
@@ -2,5 +2,19 @@
 {
     class SinglyLinkedListNode<T>
     {
+        public T Data { get; set; }
+
+        public SinglyLinkedListNode<T> Next { get; set; }
+
+        public SinglyLinkedListNode(T data)
+        {
+            Data = data;
+        }
+
+        public SinglyLinkedListNode(T data, SinglyLinkedListNode<T> next)
+        {
+            Data = data;
+            Next = next;
+        }
     }
 }
