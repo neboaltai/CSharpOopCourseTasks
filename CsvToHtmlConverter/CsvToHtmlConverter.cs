477e64f5f76ab2340a4c43d5ae6f3a6c83e6af4a
Misha Blinov <spasibo1989@gmail.com>
2022-03-22 13:56:36 +0700

    Добавлен try-catch

--- CsvToHtmlConverter/CsvToHtmlConverter.cs
+++ CsvToHtmlConverter/CsvToHtmlConverter.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 
 namespace CsvToHtmlConverter
 {
@@ -6,115 +7,123 @@ namespace CsvToHtmlConverter
     {
         public static void ConvertCsvToHtml(string inputFilePath, string outputFilePath)
         {
-            using StreamReader reader = new StreamReader(inputFilePath);
-
-            using StreamWriter writer = new StreamWriter(outputFilePath);
-            
-            writer.WriteLine("<!DOCTYPE html>");
-            writer.WriteLine("<html lang=\"ru-RU\">");
-            writer.WriteLine("\t<head>");
-            writer.WriteLine("\t\t<meta charset=\"UTF-8\">");
-            writer.WriteLine("\t\t<title>Table</title>");
-            writer.WriteLine("\t</head>");
-            writer.WriteLine("\t<body>");
-            writer.WriteLine("\t\t<table>");
-
-            string inputLine;
-            bool isNextCell = true;
-            bool isQuotes = false;
-
-            while ((inputLine = reader.ReadLine()) != null)
+            try
             {
-                if (string.IsNullOrEmpty(inputLine))
-                {
-                    continue;
-                }
+                using StreamReader reader = new StreamReader(inputFilePath);
 
-                if (isNextCell)
-                {
-                    writer.WriteLine("\t\t\t<tr>");
-                    writer.Write("\t\t\t\t<td>");
-                }
-                else
-                {
-                    writer.Write("\t\t\t\t\t");
-                }
+                using StreamWriter writer = new StreamWriter(outputFilePath);
 
-                foreach (char character in inputLine)
+                writer.WriteLine("<!DOCTYPE html>");
+                writer.WriteLine("<html lang=\"ru-RU\">");
+                writer.WriteLine("\t<head>");
+                writer.WriteLine("\t\t<meta charset=\"UTF-8\">");
+                writer.WriteLine("\t\t<title>Table</title>");
+                writer.WriteLine("\t</head>");
+                writer.WriteLine("\t<body>");
+                writer.WriteLine("\t\t<table>");
+
+                string inputLine;
+                bool isNextCell = true;
+                bool isQuotes = false;
+
+                while ((inputLine = reader.ReadLine()) != null)
                 {
-                    if (char.IsLetterOrDigit(character))
+                    if (string.IsNullOrEmpty(inputLine))
                     {
-                        writer.Write(character);
-
                         continue;
                     }
 
-                    switch (character)
+                    if (isNextCell)
                     {
-                        case '"':
-                            if (!isNextCell || isQuotes)
-                            {
-                                if (isQuotes)
+                        writer.WriteLine("\t\t\t<tr>");
+                        writer.Write("\t\t\t\t<td>");
+                    }
+                    else
+                    {
+                        writer.Write("\t\t\t\t\t");
+                    }
+
+                    foreach (char character in inputLine)
+                    {
+                        if (char.IsLetterOrDigit(character))
+                        {
+                            writer.Write(character);
+
+                            continue;
+                        }
+
+                        switch (character)
+                        {
+                            case '"':
+                                if (!isNextCell || isQuotes)
                                 {
-                                    writer.Write("\"");
+                                    if (isQuotes)
+                                    {
+                                        writer.Write("\"");
+                                    }
+
+                                    isQuotes = !isQuotes;
                                 }
 
-                                isQuotes = !isQuotes;
-                            }
+                                isNextCell = !isNextCell;
 
-                            isNextCell = !isNextCell;
+                                break;
 
-                            break;
+                            case ',':
+                                if (isNextCell)
+                                {
+                                    isQuotes = false;
 
-                        case ',':
-                            if (isNextCell)
-                            {
-                                isQuotes = false;
+                                    writer.WriteLine("</td>");
+                                    writer.Write("\t\t\t\t<td>");
+                                }
+                                else
+                                {
+                                    writer.Write(",");
+                                }
 
-                                writer.WriteLine("</td>");
-                                writer.Write("\t\t\t\t<td>");
-                            }
-                            else
-                            {
-                                writer.Write(",");
-                            }
+                                break;
 
-                            break;
+                            case '<':
+                                writer.Write("&lt;");
+                                break;
 
-                        case '<':
-                            writer.Write("&lt;");
-                            break;
+                            case '>':
+                                writer.Write("&gt;");
+                                break;
 
-                        case '>':
-                            writer.Write("&gt;");
-                            break;
+                            case '&':
+                                writer.Write("&amp;");
+                                break;
 
-                        case '&':
-                            writer.Write("&amp;");
-                            break;
+                            default:
+                                writer.Write(character);
+                                break;
+                        }
+                    }
 
-                        default:
-                            writer.Write(character);
-                            break;
+                    if (isNextCell)
+                    {
+                        isQuotes = false;
+
+                        writer.WriteLine("</td>");
+                        writer.WriteLine("\t\t\t</tr>");
+                    }
+                    else
+                    {
+                        writer.WriteLine("<br/>");
                     }
                 }
 
-                if (isNextCell)
-                {
-                    isQuotes = false;
-
-                    writer.WriteLine("</td>");
-                    writer.WriteLine("\t\t\t</tr>");
-                }
-                else
-                {
-                    writer.WriteLine("<br/>");
-                }
+                writer.WriteLine("\t\t</table>");
+                writer.WriteLine("\t</body>");
+                writer.WriteLine("</html>");
             }
-
-            writer.WriteLine("\t\t</table>");
-            writer.WriteLine("\t</body>");
-            writer.WriteLine("</html>");
+            catch (IOException e)
+            {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e);
+            }
         }
     }
 }
