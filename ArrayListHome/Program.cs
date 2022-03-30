98e83eb9e18973f83b92ff614a8691d6d476cca0
Misha Blinov <spasibo1989@gmail.com>
2022-03-31 03:00:42 +0700

    Исправил Program.cs 1. Исправил вывод сообщения об ошибке в блоке catch 2. Поправил имена переменных 3. Добавил операцию декремента во вторую подзадачу 4. Задал начальную вместимость списка и изменил цикл for в третьей подзадаче

--- ArrayListHome/Program.cs
+++ ArrayListHome/Program.cs
@@ -12,51 +12,53 @@ namespace ArrayListHome
             {
                 using StreamReader reader = new StreamReader("..\\..\\..\\input.txt");
 
-                List<string> stringList = new List<string>();
+                List<string> stringsList = new List<string>();
 
                 string currentLine;
 
                 while ((currentLine = reader.ReadLine()) != null)
                 {
-                    stringList.Add(currentLine);
+                    stringsList.Add(currentLine);
                 }
 
                 Console.WriteLine("1. Список строк из файла:");
-                Console.WriteLine(string.Join(Environment.NewLine, stringList));
+                Console.WriteLine(string.Join(Environment.NewLine, stringsList));
             }
             catch (IOException e)
             {
                 Console.WriteLine("The file could not be read:");
-                Console.WriteLine(e);
+                Console.WriteLine(e.Message);
             }
 
-            List<int> integerList = new List<int> { 1, 2, 3, 4, 5, 5, 6, 3, 7, 1, 8, 9, 0 };
+            List<int> integersList = new List<int> { 1, 2, 3, 4, 5, 5, 6, 3, 7, 1, 8, 9, 0 };
 
-            for (int i = 0; i < integerList.Count; i++)
+            for (int i = 0; i < integersList.Count; i++)
             {
-                if (integerList[i] % 2 == 0)
+                if (integersList[i] % 2 == 0)
                 {
-                    integerList.RemoveAt(i);
+                    integersList.RemoveAt(i);
+
+                    i--;
                 }
             }
 
             Console.WriteLine();
             Console.WriteLine("2. Список нечётных чисел:");
-            Console.WriteLine(string.Join(", ", integerList));
+            Console.WriteLine(string.Join(", ", integersList));
 
-            List<int> dictinctIntegerList = new List<int>();
+            List<int> distinctIntegersList = new List<int>(integersList.Count);
 
-            for (int i = 0; i < integerList.Count; i++)
+            foreach (int e in integersList)
             {
-                if (!dictinctIntegerList.Contains(integerList[i]))
+                if (!distinctIntegersList.Contains(e))
                 {
-                    dictinctIntegerList.Add(integerList[i]);
+                    distinctIntegersList.Add(e);
                 }
             }
 
             Console.WriteLine();
             Console.WriteLine("3. Список уникальных чисел:");
-            Console.WriteLine(string.Join(", ", dictinctIntegerList));
+            Console.WriteLine(string.Join(", ", distinctIntegersList));
         }
     }
 }
