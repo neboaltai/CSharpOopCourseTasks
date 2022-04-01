b91a65b21fa439975744f62df64e9fc78539d37e
Misha Blinov <spasibo1989@gmail.com>
2022-04-02 01:06:04 +0700

    Реализован класс SinglyLinkedList Добавлены члены и конструкторы

--- ListTask/SinglyLinkedList.cs
+++ ListTask/SinglyLinkedList.cs
@@ -1,6 +1,23 @@
-﻿namespace ListTask
+﻿using System;
+
+namespace ListTask
 {
     class SinglyLinkedList<T>
     {
+        public SinglyLinkedListNode<T> Head { get; private set; }
+
+        public int Count { get; private set; }
+
+        public SinglyLinkedList() { }
+
+        public SinglyLinkedList(SinglyLinkedListNode<T> head)
+        {
+            Head = head;
+
+            for (SinglyLinkedListNode<T> node = head; node != null; node = node.Next)
+            {
+                Count++;
+            }
+        }
     }
 }
