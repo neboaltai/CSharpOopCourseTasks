b15776192b58939467be749681d4c19af31f3196
Misha Blinov <spasibo1989@gmail.com>
2022-04-02 01:11:21 +0700

    Добавлен метод удаления элемента по индексу

--- ListTask/SinglyLinkedList.cs
+++ ListTask/SinglyLinkedList.cs
@@ -62,5 +62,53 @@ namespace ListTask
 
             return oldValue;
         }
+
+        public T RemoveAt(int index)
+        {
+            if (index < 0 || index >= Count)
+            {
+                throw new ArgumentOutOfRangeException(nameof(index), $"Parameter value {index} is invalid. The index must be between 0 and {Count - 1} inclusive");
+            }
+
+            SinglyLinkedListNode<T> currentNode = Head;
+
+            SinglyLinkedListNode<T> previousNode = null;
+
+            for (int i = 0; i < index; i++)
+            {
+                previousNode = currentNode;
+
+                currentNode = currentNode.Next;
+            }
+
+            T value = currentNode.Data;
+
+            if (currentNode.Next == null)
+            {
+                if (previousNode == null)
+                {
+                    Head = null;
+                }
+                else
+                {
+                    previousNode.Next = null;
+                }
+            }
+            else
+            {
+                if (previousNode == null)
+                {
+                    Head = currentNode.Next;
+                }
+                else
+                {
+                    previousNode.Next = currentNode.Next;
+                }
+            }
+
+            Count--;
+
+            return value;
+        }
     }
 }
