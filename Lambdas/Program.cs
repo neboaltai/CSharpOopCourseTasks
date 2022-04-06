0b5c786c4c5a79cb189ddbfad53a695324b98383
Misha Blinov <spasibo1989@gmail.com>
2022-04-06 14:46:43 +0700

    Создан список людей

--- Lambdas/Program.cs
+++ Lambdas/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Lambdas
 {
@@ -6,7 +8,20 @@ namespace Lambdas
     {
         static void Main(string[] args)
         {
-
+            List<Person> persons = new List<Person>
+            {
+                new Person("Sayan", 17),
+                new Person("Zhanna", 19),
+                new Person("Vladimir", 14),
+                new Person("Anna", 21),
+                new Person("Nikolay", 25),
+                new Person("Daniel", 56),
+                new Person("Ekaterina", 42),
+                new Person("Dmitry", 24),
+                new Person("Aleksander", 16),
+                new Person("Dmitry", 23),
+                new Person("Anna", 20)
+            };
         }
     }
 }
