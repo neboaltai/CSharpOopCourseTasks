4ca6d2bfee54185c735055589b49d8f3d1c34e06
Misha Blinov <spasibo1989@gmail.com>
2022-04-09 20:45:42 +0700

    Добавлены в класс List реализация интерфейса IList, поля и конструкторы

--- ArrayListTask/List.cs
+++ ArrayListTask/List.cs
@@ -1,6 +1,81 @@
-﻿namespace ArrayListTask
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ArrayListTask
 {
-    class List<T>
+    class List<T> : IList<T>
     {
+        private T[] items;
+
+        private int count;
+
+        public int Count => throw new NotImplementedException();
+
+        public T this[int index] { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public bool IsReadOnly => throw new NotImplementedException();
+
+        public List() : this(10) { }
+
+        public List(int capacity)
+        {
+            if (capacity < 0)
+            {
+                throw new ArgumentException($"Parameter value {capacity} is invalid. List capacity must not be negative", nameof(capacity));
+            }
+
+            items = new T[capacity];
+        }
+
+        public void Add(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int IndexOf(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(int index, T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveAt(int index)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
