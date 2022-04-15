22a5fae9ef7b1c8726255cd24faeaa2a935929d5
Misha Blinov <spasibo1989@gmail.com>
2022-04-15 14:32:48 +0700

    Реализован ICollection<T> в классе HashTable

--- HashTableTask/HashTable.cs
+++ HashTableTask/HashTable.cs
@@ -1,6 +1,48 @@
-﻿namespace HashTableTask
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace HashTableTask
 {
-    class HashTable
+    class HashTable<T> : ICollection<T>
     {
+        public int Count => throw new NotImplementedException();
+
+        public bool IsReadOnly => throw new NotImplementedException();
+
+        public void Add(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(T item)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
