d502060566794e7c96e7df7c57d5e4ce703f74d8
Misha Blinov <spasibo1989@gmail.com>
2022-04-28 19:45:31 +0700

    Реализован класс TreeNode

--- TreeTask/TreeNode.cs
+++ TreeTask/TreeNode.cs
@@ -1,6 +1,21 @@
 ﻿namespace TreeTask
 {
-    class TreeNode
+    class TreeNode<T>
     {
+        public T Data { get; }
+
+        public TreeNode<T> Left { get; set; }
+
+        public TreeNode<T> Right { get; set; }
+
+        public TreeNode(T data) : this(data, null, null) { }
+
+        public TreeNode(T data, TreeNode<T> left, TreeNode<T> right)
+        {
+            Data = data;
+
+            Left = left;
+            Right = right;
+        }
     }
 }
