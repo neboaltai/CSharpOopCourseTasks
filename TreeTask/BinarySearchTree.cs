a632cdb343c1f61123133d1e5c297735d7f6b135
Misha Blinov <spasibo1989@gmail.com>
2022-04-28 20:18:29 +0700

    Добавлены в класс BinarySearchTree поля и конструкторы

--- TreeTask/BinarySearchTree.cs
+++ TreeTask/BinarySearchTree.cs
@@ -1,6 +1,25 @@
-﻿namespace TreeTask
+﻿using System;
+using System.Collections.Generic;
+
+namespace TreeTask
 {
-    class BinarySearchTree
+    public class BinarySearchTree<T>
     {
+        private readonly IComparer<T> comparer;
+
+        private TreeNode<T> root;
+
+        public int Count { get; private set; }
+
+        public BinarySearchTree() { }
+
+        public BinarySearchTree(T data) : this(data, null) { }
+
+        public BinarySearchTree(T data, IComparer<T> comparer)
+        {
+            root = new TreeNode<T>(data);
+
+            this.comparer = comparer;
+        }
     }
 }
