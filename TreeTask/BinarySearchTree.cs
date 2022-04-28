c765151113227ec5d96646c58d3d411c6eebaa5a
Misha Blinov <spasibo1989@gmail.com>
2022-04-28 20:30:59 +0700

    Добавлены методы проверки и добавления элемента

--- TreeTask/BinarySearchTree.cs
+++ TreeTask/BinarySearchTree.cs
@@ -21,5 +21,64 @@ namespace TreeTask
 
             this.comparer = comparer;
         }
+
+        private void CheckDataComparable()
+        {
+            if (comparer is null && root.Data as IComparable<T> is null)
+            {
+                throw new InvalidOperationException("Data cannot be compared");
+            }
+        }
+
+        private int GetSign(T data1, T data2)
+        {
+            return comparer != null ? comparer.Compare(data1, data2) : (data1 as IComparable<T>).CompareTo(data2);
+        }
+
+        public void Add(T data)
+        {
+            if (root is null)
+            {
+                root = new TreeNode<T>(data);
+
+                Count++;
+
+                return;
+            }
+
+            CheckDataComparable();
+
+            TreeNode<T> currentNode = root;
+
+            while (true)
+            {
+                if (GetSign(currentNode.Data, data) > 0)
+                {
+                    if (currentNode.Left is null)
+                    {
+                        currentNode.Left = new TreeNode<T>(data);
+
+                        Count++;
+
+                        return;
+                    }
+
+                    currentNode = currentNode.Left;
+
+                    continue;
+                }
+
+                if (currentNode.Right is null)
+                {
+                    currentNode.Right = new TreeNode<T>(data);
+
+                    Count++;
+
+                    return;
+                }
+
+                currentNode = currentNode.Right;
+            }
+        }
     }
 }
