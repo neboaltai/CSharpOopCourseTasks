93734ec46224c9c0e09fd85a0a8389fb409ce0c1
Misha Blinov <spasibo1989@gmail.com>
2022-05-06 23:50:34 +0700

    Добавление класса конвертера, его полей и метода

---
+++ Temperature/Model/TemperatureConverter.cs
@@ -0,0 +1,34 @@
+﻿using Temperature.Model.Scales;
+
+namespace Temperature.Model
+{
+    public class TemperatureConverter
+    {
+        public IScale[] Scales { get; }
+
+        public TemperatureConverter(IScale[] scales)
+        {
+            Scales = scales;
+        }
+
+        public double Convert(IScale inputScale, IScale outputScale, double degrees)
+        {
+            if (inputScale is Celsius)
+            {
+                if (outputScale is Celsius)
+                {
+                    return degrees;
+                }
+
+                return outputScale.ConvertFromCelsius(degrees);
+            }
+
+            if (outputScale is Celsius)
+            {
+                return inputScale.ConvertToCelsius(degrees);
+            }
+
+            return outputScale.ConvertFromCelsius(inputScale.ConvertToCelsius(degrees));
+        }
+    }
+}
