2fb3913714694a09fd0573d38b35bb2d89104d58
Misha Blinov <spasibo1989@gmail.com>
2022-05-06 23:49:47 +0700

    Добавление шкалы Кельвина

---
+++ Temperature/Model/Scales/Kelvin.cs
@@ -0,0 +1,15 @@
+﻿namespace Temperature.Model.Scales
+{
+    class Kelvin : IScale
+    {
+        public double ConvertFromCelsius(double celsiusDegrees)
+        {
+            return celsiusDegrees + 273.15;
+        }
+
+        public double ConvertToCelsius(double kelvinDegrees)
+        {
+            return kelvinDegrees - 273.15;
+        }
+    }
+}
