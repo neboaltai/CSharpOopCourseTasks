2f3135cef8ddb6382585eb8a5a4375893861ea9a
Misha Blinov <spasibo1989@gmail.com>
2022-05-06 23:49:21 +0700

    Добавление шкалы Цельсия

---
+++ Temperature/Model/Scales/Celsius.cs
@@ -0,0 +1,15 @@
+﻿namespace Temperature.Model.Scales
+{
+    class Celsius : IScale
+    {
+        public double ConvertFromCelsius(double celsiusDegrees)
+        {
+            return celsiusDegrees;
+        }
+
+        public double ConvertToCelsius(double celsiusDegrees)
+        {
+            return celsiusDegrees;
+        }
+    }
+}
